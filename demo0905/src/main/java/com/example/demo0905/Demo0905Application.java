178f16bf101d1b1364814d3fe0f486aa2ea97798
刘猛 <1012954479@qq.com>
2020-09-05 11:13:39 +0800

    第一次修改

---
+++ demo0905/src/main/java/com/example/demo0905/Demo0905Application.java
@@ -0,0 +1,13 @@
+package com.example.demo0905;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo0905Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo0905Application.class, args);
+    }
+
+}
---
+++ demo0905/src/test/java/com/example/demo0905/Demo0905ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo0905;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo0905ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
